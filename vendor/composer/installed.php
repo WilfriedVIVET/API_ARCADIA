4270b8767d66ee8233f2d50549e1e57e91b248aa
WilfriedVIVET <vivetlec@outlook.fr>
2024-06-03 09:59:18 +0200

    modif composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vivet/api-arcadia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '92dcdca5d172424cf6edc1c80191965a8a56d196',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vivet/api-arcadia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '92dcdca5d172424cf6edc1c80191965a8a56d196',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

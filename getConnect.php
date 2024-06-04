9ffce2f787dd3bdae6427ad0dd650df8a5b74718
WilfriedVIVET <vivetlec@outlook.fr>
2024-06-04 12:21:44 +0200

    getConnect

--- getConnect.php
+++ getConnect.php
@@ -5,55 +5,32 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 
-function loadEnv($path)
-{
-    if (!file_exists($path)) {
-        throw new Exception("Le fichier .env n'existe pas.");
-    }
-
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) {
-            continue;
-        }
-
-        list($name, $value) = explode('=', $line, 2);
-        $name = trim($name);
-        $value = trim($value);
-
-        if (!array_key_exists($name, $_ENV)) {
-            $_ENV[$name] = $value;
-        }
-    }
-}
-
-// Détection de l'environnement
-$isLocalhost = in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']);
-$envFile = $isLocalhost ? '.env.developpement' : '.env.production';
-
-// Chargement des variables d'environnement depuis le fichier .env
-loadEnv(__DIR__ . '/'. $envFile);
-
-// Gérer les requêtes OPTIONS (pré-vol CORS)
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit;
+if(getenv('JAWSDB_URL') !== false){
+    $dbparts = parse_url(getenv('JAWSDB_URL'));
+    //En ligne
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+}else{
+    //En local
+    $username = 'root';
+    $password = '';
+    $database = 'arcadia';
+    $hostname= 'localhost';
 }
 
 // Fonction de connexion à la base de données
-function getConnect(){
-    $dsn = $_ENV['DB_DSN'];
-    sendJson($dsn);
+function getConnect($hostname,$username,$password,$database){
+   
     try {
-        $pdo = new PDO($dsn);
+        $pdo = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
         // l'erreur de connexion.
         throw new Exception("Erreur de connexion à la base de données: " . $e->getMessage());
-    } catch (Exception $e) {
-        throw new Exception("Erreur de connexion à la base de données: " . $e->getMessage());
-    }
+    } 
 }
 
 
